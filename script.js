08fe9fdc9f5f77acd3bc480dac3e7ec8ebe8723c
georgiy-krasilnikov <grgkrslnkv@gmail.com>
2023-05-15 16:06:09 +0300

    main: add right-menu

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function SwapBackground(a_id, li_id) {
     let li = document.getElementById(li_id);
     a.style.color = 'black';
     a.style.background = 'white';
-    a.style.borderBottom = '2px solid white';
+    a.style.borderBottom = '2px solid black';
     a.style.padding = '5px 10px';
     a.style.borderRadius = '15px';
     li.style.padding = li_id == '1' && a_id == '1.1' ? '0px 40px 0px 90px' : li.style.padding = '0px 40px 0px 30px';
