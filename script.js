f88207a44a0d70984e9addd47c597522d8e95c98
georgiy-krasilnikov <grgkrslnkv@gmail.com>
2023-05-28 21:50:56 +0300

    main: add page 'school'

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ function SwapBackground(a_id, li_id) {
     a.style.borderRadius = '15px';
     li.style.padding = li_id == '1' && a_id == '1.1' ? '0px 40px 0px 90px' : li.style.padding = '0px 40px 0px 30px';
 }
+
+function HiddenBlock(event, el_id) {
+    let el = document.getElementById(el_id);
+    if (event == 'onmouseover') {
+        el.style.transition  = 'all 1.5s ease-out';
+        el.style.opacity = '1';
+    } else {
+        el.style.opacity = '0';
+    }
+}
