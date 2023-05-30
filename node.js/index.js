9710322f267e285298c360de14bcd40e6e011e11
georgiy-krasilnikov <grgkrslnkv@gmail.com>
2023-05-30 16:45:04 +0300

    main: upd

--- node.js/index.js
+++ node.js/index.js
@@ -1,10 +1,29 @@
-var express = require("express");
-var http = require("http");
-var path = require("path");
+const http = require("http");
+const fs = require("fs").promises;
 
-var app = express();
+const host = "localhost";
+const port = 8000;
 
-app.listen(8080);
-var server = http.createServer(app);
+const requestListener = function (req, res) {
+  fs.readFile(__dirname + "/front-end/index.html")
+    .then((contents) => {
+      if (req.url.endsWith(".css")) contentType = "text/css";
+      if (req.url.endsWith(".js")) contentType = "text/javascript";
+      if (req.url.endsWith(".png")) contentType = "image/png";
+      if (req.url.endsWith(".png")) contentType = "image/jpg";
+      res.setHeader("Content-Type", contentType);
+      res.writeHead(200);
+      res.end(contents);
+    })
+    .catch((err) => {
+      res.writeHead(500);
+      res.end(err);
+      return;
+    });
+};
 
-app.use(express.static(path.join(__dirname-"/node.js", "/public")));
+const server = http.createServer(requestListener);
+
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
