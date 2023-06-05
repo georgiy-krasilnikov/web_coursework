cf2e9c30c9f730e0b1ff94ca14318dd046aae116
georgiy-krasilnikov <grgkrslnkv@gmail.com>
2023-06-05 22:30:11 +0300

    main: fix

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
-const staticPath = path.join(__dirname, "static");
+const staticPath = path.join(__dirname, "static"); //путь для стат. файлов
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "static")));
 
-app.get("/", (req, res) => res.sendFile(path.join(staticPath, "index.html")));
+app.get("/", (req, res) => res.sendFile(path.join(staticPath, "index.html"))); //главная страница
 
 app.get("/about", (req, res) =>
   res.sendFile(path.join(staticPath, "about.html"))
@@ -18,8 +18,16 @@ app.get("/news", (req, res) =>
   res.sendFile(path.join(staticPath, "news.html"))
 );
 
-app.get("/school", (req, res) => {
-  res.sendFile(path.join(staticPath, "school.html"));
-});
+app.get("/school", (req, res) =>
+  res.sendFile(path.join(staticPath, "school.html"))
+);
 
-app.listen(8000);
+app.get("/news/file", (req, res) => //запись в файл
+  fs.writeFile(
+    "file.txt",
+    "Пользователь записан!",
+    (err) => err && console.error(err)
+  )
+);
+
+app.listen(8000); //порт для сервера сайта
